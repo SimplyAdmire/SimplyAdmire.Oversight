77ca942d9a67a047b23a813eb7d5571cf1166e07
Lars Nieuwenhuizen <lars@keenvantage.nl>
2016-10-22 16:29:05 +0200

    TASK: Update styleci settings

--- Classes/SimplyAdmire/Oversight/Controller/StandardController.php
+++ Classes/SimplyAdmire/Oversight/Controller/StandardController.php
@@ -1,10 +1,6 @@
 <?php
 namespace SimplyAdmire\Oversight\Controller;
 
-/*
- * This file is part of the SimplyAdmire.Oversight package.
- */
-
 use TYPO3\Flow\Annotations as Flow;
 
 class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController
