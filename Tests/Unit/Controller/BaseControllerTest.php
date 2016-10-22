f3c38d9e2210d68b89e04a792612981b1304d40a
Lars Nieuwenhuizen <lars@keenvantage.nl>
2016-10-22 17:28:43 +0200

    FIX: Change autoloading from PSR-0 to PSR-4

---
+++ Tests/Unit/Controller/BaseControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace SimplyAdmire\Oversight\Tests\Unit\Controller;
+
+use TYPO3\Flow\Tests\BaseTestCase;
+
+class EventControllerTest extends BaseTestCase
+{
+
+    /**
+     * @test
+     */
+    public function dummyTest()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
